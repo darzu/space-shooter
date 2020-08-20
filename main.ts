778c53848e6343f03cdda36a3fac116b10f213ed
Daryl Zuniga <Daryl.Zuniga@gmail.com>
2020-08-20 11:35:25 -0700

    Add health bars to enemies

--- main.ts
+++ main.ts
@@ -18,9 +18,12 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         `, mySprite, 200, 0)
 })
+statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
+    status.spriteAttachedTo().destroy()
+})
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprite.destroy()
-    otherSprite.destroy()
+    statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite).value += -15
     info.changeScoreBy(1)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
@@ -28,6 +31,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     otherSprite.destroy(effects.disintegrate, 500)
     scene.cameraShake(4, 500)
 })
+let statusbar: StatusBarSprite = null
 let enemyShip: Sprite = null
 let projectile: Sprite = null
 let mySprite: Sprite = null
@@ -75,4 +79,8 @@ game.onUpdateInterval(2000, function () {
     enemyShip.x = scene.screenWidth()
     enemyShip.vx = -20
     enemyShip.y = randint(10, scene.screenHeight() - 10)
+    statusbar = statusbars.create(15, 2, StatusBarKind.EnemyHealth)
+    statusbar.setColor(5, 12)
+    statusbar.max = 100
+    statusbar.attachToSprite(enemyShip)
 })
